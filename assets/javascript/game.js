81d0963ccf5b6c0f4bb1df9be47b8a317fdd4015
wcrozier12 <wcc8@nau.edu>
2017-08-02 14:26:24 -0700

    game with codeacademy logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,49 +10,53 @@ var words = ["biology", "chemistry", "physics"];
 var hints = ["study of life", "study of matter", "study of the nature of matter"];
 var newWord = 0;
 var wins = 0;
-var blankSpaces = "";
-var currentWord = words[Math.floor(Math.random() * words.length)];
+var randomNumber = Math.floor(Math.random() * words.length);
+var currentWord = words[randomNumber];
+var currentHint= hints[randomNumber];
 var wordLength = currentWord.length;
-var currentHint="";
-//initiates setup function
-window.onload = function() {setup()};
+var userGuess =  "s";
+var blanks = "";
+var replace =[]; //contains locations of letters that need to be replaced
 
-//setup
-function setup() {
-	//selects random word from words array
-
-
-	//Displays possible words blank spaces
-	for (i = 0; i < wordLength; i++) {
-    	var x = currentWord.charAt(i);
-
-    if (x === " " || x === "/'") {
-      blankSpaces += x;
-    } else {
-      blankSpaces += "_ ";
-    }
-   }
-//shows hint based on word
-	if (currentWord === words[0]) {
-		currentHint += hints[0];
+function blankSpaces (currentWord) {
+	for (var i in currentWord) {
+		blanks += "_";
 	}
-	else if (currentWord === words[1]) {
-		currentHint += hints[1];
-	}
-	else if (currentWord === words[2]) {
-		currentHint += hints[2];
-	}
+	return blanks;
+}
 
-    document.getElementById("hint").innerHTML =  "Hint: " + currentHint;
 
-	document.getElementById("blankspaces").innerHTML =  "Current word: " + blankSpaces;
+function replaceAt(match, userInput) {
+	return blanks.substr(0, match) + userInput + blanks.substr(match+1, blanks.length);
+}
+
+function getReplacePositions(currentWord, userInput){
+	var searchIndex = currentWord.indexOf(userGuess);
+	while (searchIndex != -1) {
+		replace.push(searchIndex);
+		searchIndex = currentWord.indexOf(userGuess, searchIndex + 1);
+	}
+}
+function replaceLetter(){
+	var searchIndex = -1;
+	var searchIndex = currentWord.indexOf(userGuess);
+		while (searchIndex >= 0) {
+			var replaced = replaceAt(searchIndex, userGuess);	
+			searchIndex = currentWord.indexOf(userGuess, searchIndex + 1);
+		}	
+		return replaced;
+	}	
 	
-	document.onkeyup =function(event){
-	var userGuess = event.key;
-	if (userGuess )
 
-}
-}
+
+blankSpaces(currentWord);
+console.log(replaceLetter());
+
+console.log(replace);
+
+
+
+
 
 
 
